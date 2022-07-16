d09c77f268250ab1b7aa2335c69250197049e4ec
Luis Humberto <beto020017@gmail.com>
2022-07-16 01:38:05 -0500

    Initial Commit

---
+++ Caso practico UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Caso practico UIKit
+//
+//  Created by Luis Humberto Martinez Echegaray on 16/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
