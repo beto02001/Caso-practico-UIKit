1463b0e14aa66e86de89474434e20631ae55db64
Luis Humberto <beto020017@gmail.com>
2022-07-16 02:11:32 -0500

    add TableView

--- Caso practico UIKit/View/Lista Productos /ListProductosViewController.swift
+++ Caso practico UIKit/View/Lista Productos /ListProductosViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class ListProductosViewController: UIViewController {
+    
+    @IBOutlet weak var productosTableVIew: UITableView!
+    
     var productos = [Producto]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        productosTableVIew.delegate = self
+        productosTableVIew.dataSource = self
         print("en la nueva: ", productos.first?.nombre)
     }
+
+}
+
+extension ListProductosViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return productos.count
+    }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = productosTableVIew.dequeueReusableCell(withIdentifier: "cellProducto", for: indexPath)
+        return cell
+    }
+    
+    
 }
